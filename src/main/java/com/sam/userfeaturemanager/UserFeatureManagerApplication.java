b966bd1df0e26dde2c152320d22e58abdb211a79
sam <sameera.prakash@accenture.com>
2022-02-01 16:19:47 +0000

    Initial commit

---
+++ src/main/java/com/sam/userfeaturemanager/UserFeatureManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sam.userfeaturemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFeatureManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFeatureManagerApplication.class, args);
+	}
+
+}
